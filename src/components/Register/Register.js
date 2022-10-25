bc6c4c839e9f039c81911789aa9ae2d22cc68cf3
ASFahim007 <devasfahim@gmail.com>
2022-10-25 22:42:05 +0600

    register form fully done

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,8 +2,48 @@ import React from 'react';
 import './Register.css';
 import register from '../../assets/image/register.png';
 import { FaGoogle } from 'react-icons/fa';
+import { useState } from 'react';
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const Register = () => {
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(false);
+
+    const { createUser, createUserWithGoogle } = useContext(AuthContext)
+
+    // sign in with email and password 
+    const handleCreateUser = (event) => {
+        event.preventDefault()
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        setSuccess(false)
+        setError(false)
+
+
+        createUser(email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                console.log(user);
+                setSuccess(true)
+                form.reset()
+            })
+            .catch((error) => {
+                setError(error.message)
+            });
+    }
+    // sign in with google
+    const handleGoogleUser = () => {
+        createUserWithGoogle()
+        .then((result) => {
+            const user = result.user;
+            console.log(user);
+        }).catch((error) => {
+            setError(error.message)
+        });
+    }
+
     return (
         <div>
             <div className="page_banner">
@@ -20,19 +60,21 @@ const Register = () => {
                         <div className="col-lg-6">
                             <div className="connect_form">
                                 <h1>Create your account</h1>
-                                <form>
-                                    <div class="mb-3">
-                                        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Your Name" />
+                                <form onSubmit={handleCreateUser}>
+                                    <div className="mb-3">
+                                        <input type="text" className="form-control" placeholder="Your Name" required />
                                     </div>
-                                    <div class="mb-3">
-                                        <input type="email" class="form-control" id="formGroupExampleInput2" placeholder="Your Email" />
+                                    <div className="mb-3">
+                                        <input type="email" name='email' className="form-control" placeholder="Your Email" required />
                                     </div>
-                                    <div class="mb-3">
-                                        <input type="email" class="form-control" id="formGroupExampleInput2" placeholder="Your Password" />
+                                    <div className="mb-3">
+                                        <input type="password" name='password' className="form-control" placeholder="Your Password" required />
                                     </div>
+                                    {success && <p className="text-success">Successfully registered</p>}
+                                    {error && <p className="text-danger">{error}</p>}
                                     <button className="btn default-btn d-block w-100">Register Now</button>
-                                    <button className="btn mt-2 btn-outline-primary d-block w-100"> <FaGoogle className='me-3'></FaGoogle> Register with Google</button>
                                 </form>
+                                <button onClick={handleGoogleUser} className="btn mt-2 btn-outline-primary d-block w-100"> <FaGoogle  className='me-3'></FaGoogle> Register with Google</button>
                             </div>
                         </div>
                     </div>
--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import { createContext } from 'react';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth'
+import app from '../../firebase/firebase.config';
 
-export const AuthContext = createContext()
+export const AuthContext = createContext();
+
+
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider()
 
 const AuthProvider = ({children}) => {
     const user = {displayName: 'akash'}
 
-    const authInfo = {user}
     
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const userLogIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const createUserWithGoogle = () => {
+        return signInWithPopup(auth, googleProvider)
+    }
+    
+    const authInfo = {user, createUser, createUserWithGoogle, userLogIn };
+
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
